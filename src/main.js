75e0de246be99a7d0f1e0aa8d1babe6af811cb7e
truejojo <johannesveh@yahoo.de>
2024-04-20 15:46:56 +0200

    SW manifes

--- src/main.js
+++ src/main.js
@@ -13,4 +13,18 @@ app.use(createPinia());
 
 app.mount('#app');
 
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/serviceworker.js').then(
+		registration => {
+			console.log('success!');
+			if (registration.installing) {
+				registration.installing.postMessage('Howdy from your installing page.');
+			}
+		},
+		err => {
+			console.error('Installing the worker failed!', err);
+		}
+	);
+}
+
 import 'bootstrap/dist/js/bootstrap.min.js';
